ec56473f7ea31504162279aa3eac844c5d0f06c2
Ray <q8977452@gmail.com>
2020-03-14 17:35:06 +0800

    update

--- js/waterfall.js
+++ js/waterfall.js
@@ -13,53 +13,20 @@
      * 內容JSON
      */
   var demoContent = [
-    {
-      demo_link: 'http://chanceat.azurewebsites.net/Login/Index',
-      img_link: 'http://chanceat.azurewebsites.net/Content/img/logo.png',
-      title: '晨食餐飲集團',
-      core_tech: 'ASP.NET MVC',
-      description: '資訊工業策進會結訓專題'
-    },
-
-    {
-      demo_link: 'https://tad.tycg.gov.tw/',
-      img_link: 'https://tad.tycg.gov.tw/images/bg_index_banner.png',
-      title: '桃園市政府交通事件裁決處',
-      core_tech: 'ASP.NET MVC',
-      description: '桃園市政府交通事件裁決處，線上申辦、便民服務'
-    },
+   
     
-    {
-      demo_link: 'https://epaanni.epa.gov.tw/',
-      img_link: 'https://epaanni.epa.gov.tw/images/img_index_banner.jpg',
-      title: '環保署30週年慶',
-      core_tech: 'ASP.NET MVC',
-      description: '行政院環保署30週年慶，各局處大事記、各地今昔對比、多媒體圖片影音電子書。'
-    },
 
-    {
-      demo_link: 'http://miaolitravel.net/',
-      img_link: 'http://miaolitravel.net/images/miaoli_body_image3.gif',
-      title: '苗栗玩透透',
-      core_tech: 'ASP.NET MVC',
-      description: '苗栗文化觀光旅遊網、苗栗、文化、觀光、旅遊、桐花、溫泉、單車、自由行、住宿、餐飲、美食、風景、螢火蟲、木雕、藝術、水果、草莓、邯鄲、元宵、火旁龍、客家'
-    },
+   
 
-    {
-      demo_link: 'https://www.web-fd.taipei/i-AMOUR/',
-      img_link: 'https://www.web-fd.taipei/i-AMOUR/images/stories/site/inner_banner2.png',
-      title: 'i-Amour 愛上視障按摩',
-      core_tech: 'ASP.NET MVC',
-      description: '視障按摩在臺灣是一種非常特別的行業，在過去，只有視障者能夠從事按摩的工作，但在100年11月1日起因為大法官649號釋憲開放了按摩市場，按摩工作不再專屬於視障朋友，所以政府有一些輔導措施（包括按摩推廣行銷、技術養成與在職訓練、按摩環境更新改善、優良按摩院甄選以及創業補助等）協助視障按摩師重新包裝自己，讓社會大眾對於視障按摩有全新的體認。'
-    },
+   
 
-    {
+    /* {
       demo_link: 'https://agriexpo.tycg.gov.tw/',
       img_link: 'https://agriexpo.tycg.gov.tw/Images/Fin/0.jpg',
       title: '桃園農業博覽會',
       core_tech: 'ASP.NET MVC',
       description: '「2017桃園農業博覽會」雖然從試營運出發，但卻創下同質性活動中超高入園人數的佳績，更成為全臺矚目的焦點！桃園市政府秉持專業、效率、為民服務的精神，在市府團隊的同心協力下，完成無數不可能的任務！更帶動地方、產業及中央的共同關注與支持！展望「2018桃園農業博覽會」正式營運市府將透過深耕在地文化、展現產業創新、帶動全民參與提升國際能見度等，藉以凝聚桃園的力量、拉近城鄉的距離、共同推展桃園智慧農業、永續家園的城市新願景！'
-    }
+    } */
   ];
 
   contentInit(demoContent) //內容初始化
